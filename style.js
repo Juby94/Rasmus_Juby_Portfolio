613316243c2e657884e6aa395c195340805f65e8
Juby94 <31645864+Juby94@users.noreply.github.com>
2018-01-08 01:37:06 +0100

    Portfolio done

---
+++ style.js
@@ -0,0 +1,64 @@
+/*** Burger Menu ***/
+$(".dropbtn").on("click", myFunction);
+
+function myFunction() {
+
+    document.getElementById("myDropdown").classList.toggle("show");
+    $(".bar1").addClass("change1");
+    $(".bar2").addClass("change2");
+    $(".bar3").addClass("change3");
+
+}
+
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+                $(".bar1").removeClass("change1");
+                $(".bar2").removeClass("change2");
+                $(".bar3").removeClass("change3");
+            }
+        }
+    }
+}
+/*** Burger menu slut ***/
+/*** Baggrund ***/
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+    x += (lFollowX - x) * friction;
+    y += (lFollowY - y) * friction;
+
+    translate = 'translate(' + x + 'px, ' + y + 'px)';
+
+    $('.background_img').css({
+        '-webit-transform': translate,
+        '-moz-transform': translate,
+        'transform': translate
+    });
+
+    window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function (e) {
+
+    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+    lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+    lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
+/*** Baggrund slut ***/
